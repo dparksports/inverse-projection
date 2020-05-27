99dddab2848d744c096770146c99c0e18bb28054
Dan Park <dpark.sports@gmail.com>
2020-05-26 21:15:38 -0700

    added orthogonal projection

--- inverseprojection.py
+++ inverseprojection.py
@@ -1,10 +1,37 @@
 import open3d as o3d
 import cv2
-import matplotlib
+import matplotlib.pyplot as plt
 import scipy.linalg as lin
 import numpy as np
 
-def homogenous_image_coordinates(h, w) :
+def orthogonal_view(camera_points):
+    longitunal_max = 70
+    x_window = (-50, 50)
+    y_window = (-3, longitunal_max)
+
+    x, y, z = camera_points
+    y = -1 * y
+
+    # In an ego vehicle, the observing camera is placed 1 meter top of the roof top.
+    points_with_70meters = np.where((z < longitunal_max) & (y > -1.2))
+    z_based_view = camera_points[:3, points_with_70meters]
+
+    # color points by radial distance
+    distances = np.sqrt(np.sum(z_based_view[0:2:2, :] ** 2, axis=0))
+    axex_limit = 10
+    color_array = np.minimum(1, distances / axex_limit / np.sqrt(2))
+
+    fig, axes = plt.subplots(figsize=(12, 12))
+    axes.scatter(z_based_view[0, :], z_based_view[2, :], c=color_array, s=0.1)
+    axes.set_xlim(x_window)
+    axes.set_ylim(y_window)
+
+    plt.axis('off')
+    plt.gca().set_aspect('equal')
+    plt.show()
+
+
+def homogenous_image_coordinates(h, w):
     """
     image coordinates in homogenous coordinate
     :param h:
@@ -16,7 +43,7 @@ def homogenous_image_coordinates(h, w) :
     [x, y] = np.meshgrid(x, y)
     return np.vstack((x.flatten(), y.flatten(), np.ones_like(x.flatten())))
 
-def intrinsic_from_fov(h, w, fov=90) :
+def intrinsic_from_fov(h, w, fov=90):
     px, py = w / 2.0, h / 2.0
     fov_h = fov / 360.0 * 2.0 * np.pi
     focal_length_x = w / (2.0 * np.tan (fov_h / 2.0))
@@ -41,6 +68,8 @@ camera_coordinates = camera_coordinates[:, np.where(camera_coordinates[2] < 150)
 pointcloud_in_camera_coordinates = o3d.geometry.PointCloud()
 pointcloud_in_camera_coordinates.points = o3d.utility.Vector3dVector(camera_coordinates.T[:, :3])
 o3d.visualization.draw_geometries([pointcloud_in_camera_coordinates])
+
+orthogonal_view(camera_coordinates)
 
 
 
